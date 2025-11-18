6ab4731768ba968a386eb944fa79e07fa720959a
AkhileshTalekar <akhilesh.talekar21@gmail.com>
2025-11-18 19:51:09 +0530

    feat: initialize frontend project with Vite and React
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Include Vite logo SVG in public directory.
    - Create App.css for styling the application layout and components.
    - Implement App.tsx as the main application component with data fetching using Axios.
    - Add React logo SVG in assets directory.
    - Set up global styles in index.css for consistent theming and layout.
    - Create main.tsx to bootstrap the React application.
    - Configure TypeScript with tsconfig.app.json, tsconfig.node.json, and tsconfig.json for proper type checking and module resolution.
    - Add Vite configuration in vite.config.ts to integrate React plugin.

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("http://localhost:8080/api/data");
+      setData(data.message);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{data ? data : "Loading..."}</h1>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
